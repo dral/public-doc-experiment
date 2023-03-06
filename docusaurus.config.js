04748a4152a7331f7f98309de19623d5c94ec42f
Draltan Marin <moi@draltan.com>
2023-03-06 13:07:35 +0100

    fix: documentation loading problem

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -14,7 +14,7 @@ const config = {
   url: 'https://dral.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/public-doc-experiment/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
